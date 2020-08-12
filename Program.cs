c3f51834fca1bb3049e0af9460ef23f0ad4faa41
Zachary Turner <zacharyturner@Zacharys-MBP.home>
2020-08-12 00:15:10 -0400

    I created variables and converted strings to doubles for a math equation

--- Program.cs
+++ Program.cs
@@ -2,11 +2,41 @@
 
 namespace VariablesCS
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            var NumberOfCupsOfCoffee = 2;
+            var fullName = "Zachary Turner";
+            var todaysDate = "8/11/2020";
+
+            Console.WriteLine(fullName + " " + NumberOfCupsOfCoffee + " " + todaysDate);
+
+            Console.WriteLine("What is your name?");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine("nice to meet you " + userName);
+
+            Console.WriteLine("Give me an even number");
+            var firstNumberString = Console.ReadLine();
+
+            Console.WriteLine("Give me an odd number");
+            var secondNumberString = Console.ReadLine();
+
+            var firstOperand = double.Parse(firstNumberString);
+            var secondOperand = double.Parse(secondNumberString);
+
+            var sum = (firstOperand + secondOperand);
+            var difference = (firstOperand - secondOperand);
+            var product = (firstOperand * secondOperand);
+            var quotient = (firstOperand / secondOperand);
+            var remainder = (firstOperand % secondOperand);
+
+            Console.WriteLine("sum is " + sum);
+            Console.WriteLine("difference is " + difference);
+            Console.WriteLine("product is " + product);
+            Console.WriteLine("quotient is " + quotient);
+            Console.WriteLine("remainder is " + remainder);
+        }
     }
-  }
 }
